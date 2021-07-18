eb208d215311e2f009ed35406d2fa4c0ec6cc983
Murat <bern.murat@gmail.com>
2021-07-18 10:23:55 +0200

    password.py added to assignments

--- Python/AssignmentsTasks/password.py
+++ Python/AssignmentsTasks/password.py
@@ -2,6 +2,6 @@ dict_ = {"Joseph":"W@12"}
 string_ = input("To see your password write your username correctly : ").strip().title()
 
 if string_ == "Joseph":
-    print(f"Hello, {string_}! The password is : {dict_[string_]}")
+    print(f"Hello, {string_}!The password is : {dict_[string_]}")
 else:
     print(f"Hello, {string_}.See you later.")
