e1cf3d74fa9852201d748cb44a7c8ab9acaa855f
Murat <bern.murat@gmail.com>
2021-07-18 10:16:31 +0200

    ClassNotes/zip_method.odt added

---
+++ Python/AssignmentsTasks/password.py
@@ -0,0 +1,7 @@
+dict_ = {"Joseph":"W@12"}
+string_ = input("To see your password write your username correctly : ").strip().title()
+
+if string_ == "Joseph":
+    print(f"Hello, {string_}! The password is : {dict_[string_]}")
+else:
+    print(f"Hello, {string_}.See you later.")
