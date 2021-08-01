db083da87029d96aedaf66f030a443ea74dea22c
Mrt-GitHub <bern.murat@gmail.com>
2021-08-01 20:07:54 +0200

    covid.py assigment made

--- Python/AssignmentsTasks/Covid.py
+++ Python/AssignmentsTasks/Covid.py
@@ -1,1 +1,7 @@
-
+age =  (input("Are you a cigarette addict older than 75 years old?").lower().strip() == "yes")
+chronic = (input("Do you have a severe chronic disease?").lower().strip() == "yes")
+immune = (input("Is your immune system too weak?").lower().strip() == "yes")
+if age and chronic and immune:
+    print("You are in risky group")
+else:
+    print("You are not in risky group")
